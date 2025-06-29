227dc912ed66cefa7b147e8f3036b0d1b68de00f
yongjun <1640808365@qq.com>
2025-06-29 15:35:09 +0800

    dubbo基础使用完成

--- yw-live-api/src/main/java/com/yw/live/aip/controller/TestController.java
+++ yw-live-api/src/main/java/com/yw/live/aip/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.yw.live.aip.controller;
 
+import com.yw.live.user.interfaces.IUserRpc;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class TestController {
 
+    @DubboReference
+    private IUserRpc userRpc;
+
     @RequestMapping("/sayHello/{name}")
     public void sayHello(@PathVariable(name = "name") String name) {
         System.out.println("api:"+name);
-//        userRpc.sayHello(name);
+        userRpc.sayHello(name);
     }
 }
