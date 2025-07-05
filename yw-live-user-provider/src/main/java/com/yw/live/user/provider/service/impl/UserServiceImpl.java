3cb4613c14ad4467c72601e5f8668d729c71c024
yongjun <1640808365@qq.com>
2025-07-05 17:00:32 +0800

    redis配置初始化及使用

--- yw-live-user-provider/src/main/java/com/yw/live/user/provider/service/impl/UserServiceImpl.java
+++ yw-live-user-provider/src/main/java/com/yw/live/user/provider/service/impl/UserServiceImpl.java
@@ -6,17 +6,31 @@ import com.yw.live.user.dto.UserDTO;
 import com.yw.live.user.provider.dao.mapper.IUserMapper;
 import com.yw.live.user.provider.dao.po.UserPO;
 import com.yw.live.user.provider.service.IUserService;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<IUserMapper, UserPO> implements IUserService {
+
+    @Resource
+    private RedisTemplate<String,UserDTO> redisTemplate;
 
     @Override
     public UserDTO getUserById(Long userId) {
         if (userId == null) {
             return null;
         }
-        return BeanUtil.copyProperties(baseMapper.selectById(userId), UserDTO.class);
+        return Optional.ofNullable(redisTemplate.opsForValue().get(String.valueOf(userId)))
+                .orElseGet(() -> {
+                    UserDTO userDTO = BeanUtil.copyProperties(baseMapper.selectById(userId), UserDTO.class);
+                    if (userDTO != null) {
+                        redisTemplate.opsForValue().set(String.valueOf(userId), userDTO);
+                    }
+                    return userDTO;
+                });
     }
 
     @Override
