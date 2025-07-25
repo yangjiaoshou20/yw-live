5c9ae0e52a086c1e59a5e63bd524d13b3eaf51e0
yongjun <1640808365@qq.com>
2025-07-25 15:12:02 +0800

    优化项目依赖

--- yw-live-user-provider/src/main/java/com/yw/live/UserProviderApplication.java
+++ yw-live-user-provider/src/main/java/com/yw/live/UserProviderApplication.java
@@ -3,7 +3,6 @@ package com.yw.live;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -11,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserProviderApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(UserProviderApplication.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);//Dubbo不使用tomcat，使用netty
         springApplication.run(args);
     }
-
 }
