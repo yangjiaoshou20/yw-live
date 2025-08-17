a4bf631bddd12a4351afc72be6efcf06f63411f9
yongjun <1640808365@qq.com>
2025-08-17 19:12:56 +0800

    启动环境变量添加及日志规范他统一

---
+++ yw-live-user-provider/src/main/java/com/yw/live/common/interfaces/utils/IpLogConversionRule.java
@@ -0,0 +1,25 @@
+package com.yw.live.common.interfaces.utils;
+
+import ch.qos.logback.core.PropertyDefinerBase;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 保证每个docker容器的日志挂载目录唯一性
+ */
+public class IpLogConversionRule extends PropertyDefinerBase {
+
+    @Override
+    public String getPropertyValue() {
+        return this.getLogIndex();
+    }
+
+    private String getLogIndex() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return String.valueOf(ThreadLocalRandom.current().nextInt(100000));
+        }
+    }
+}
